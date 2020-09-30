71b18d52586dd909ad698444cb87b0b75ed1eade
jiangsai <jiangsai@oneship.cc>
2020-09-30 11:00:23 +0800

    update

--- xxl-job-admin/src/main/resources/static/js/joblog.index.1.js
+++ xxl-job-admin/src/main/resources/static/js/joblog.index.1.js
@@ -197,8 +197,8 @@ $(function() {
 										'       <li><a href="javascript:void(0);" class="logDetail" _id="'+ row.id +'" >'+ I18n.joblog_rolling_log +'</a></li>\n' +
 										'       <li class="divider"></li>\n' +
 										'       <li><a href="javascript:void(0);" class="logKill" _id="'+ row.id +'" >'+ I18n.joblog_kill_log +'</a></li>\n' +
-										/*'       <li class="divider"></li>\n' +
-										'       <li><a href="javascript:void(0);" class="logStop" _id="'+ row.id +'" >'+ I18n.joblog_stop_log +'</a></li>\n' +*/
+										'       <li class="divider"></li>\n' +
+										'       <li><a href="javascript:void(0);" class="logStop" _id="'+ row.id +'" >'+ I18n.joblog_stop_log +'</a></li>\n' +
 										'     </ul>\n' +
 										'   </div>';
 
